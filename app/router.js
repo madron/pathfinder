250a077e4ac48401a93fc6939baad8e69103c7cb
madron <massimiliano.ravelli@gmail.com>
2017-07-06 15:26:03 +0200

    added draft for character-add route

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('character', function() {});
+  this.route('character', function() {
+    this.route('add');
+  });
 });
 
 export default Router;
---
+++ tests/acceptance/character-add-test.js
@@ -0,0 +1,13 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'pathfinder/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | character add');
+
+test('visiting /character-add', function(assert) {
+  visit('/character/add');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/character/add');
+    assert.equal(find('h1:first').text(), 'Add character');
+  });
+});
